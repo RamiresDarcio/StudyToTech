0375f2ad47cad5adcb2d9f24d2357f46a7b742d0
ramires_felino <ramiresdarcio2104@gmail.com>
2026-05-13 13:57:04 -0300

    ok

--- StudyToTechWeb/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace StudyToTechWeb.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
